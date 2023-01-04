c06a97d9efb2de5020d22bd3820d0092d6cb0c52
BartekLB <120518274+BartekLB@users.noreply.github.com>
2023-01-04 13:07:32 +0100

    Update Potega 5.cpp
    Kody funkcji 5-8

--- Potega 5.cpp
+++ Potega 5.cpp
@@ -10,3 +10,41 @@
                  cout << "2 POTEGA (KWADRAT) TWOJEJ LICZBY TO: " << kwadrat;
              }
              break;
+   // PIERWIASTEK 2 STOPNIA Z LICZBY
+             case '6':
+             {
+                 double x, sqrt2;
+                 cout << endl;
+                 cout << "WYBRALES PIERWIASTEK 2 STOPNIA Z LICZBY\n";
+                 cout << "PODAJ LICZBE: ";
+                 cin >> x;
+                 sqrt2 = pow(x, 0.5);
+                 cout << "PIERWIASTEK 2 STOPNIA Z TWOJEJ LICZBY TO: " << sqrt2;
+             }
+             break;
+
+   // 3 POTEGA (SZESCIAN LICZBY)
+             case '7':
+             {
+                 int x, szescian;
+                 cout << endl;
+                 cout << "WYBRALES 3 POTEGA (SZESCIAN LICZBY)\n";
+                 cout << "PODAJ LICZBE: ";
+                 cin >> x;
+                 szescian = x * x * x;
+                 cout << "3 POTEGA (SZESCIAN) TWOJEJ LICZBY TO: " << szescian;
+             }
+             break;
+
+  // PIERWIASTEK 3 STOPNIA Z LICZBY
+             case '8':
+             {
+                 double x, sqrt3;
+                 cout << endl;
+                 cout << "WYBRALES PIERWIASTEK 3 STOPNIA Z LICZBY\n";
+                 cout << "PODAJ LICZBE: ";
+                 cin >> x;
+                 sqrt3 = pow(x, 0.33333333333333333333333333);
+                 cout << "PIERWIASTEK 3 STOPNIA Z TWOJEJ LICZBY TO: " << sqrt3;
+             }
+             break;
