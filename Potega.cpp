e833cf01e33970725a9c8d7bac3bb9a89fcb01ce
BartekLB <120518274+BartekLB@users.noreply.github.com>
2023-01-04 13:04:30 +0100

    Create Potega.cpp

---
+++ Potega.cpp
@@ -0,0 +1,12 @@
+ // 2 POTEGA (KWADRAT LICZBY)
+             case '5':
+             {
+                 int x, kwadrat;
+                 cout << endl;
+                 cout << "WYBRALES 2 POTEGA (KWADRAT LICZBY)\n";
+                 cout << "PODAJ LICZBE: ";
+                 cin >> x;
+                 kwadrat = x * x;
+                 cout << "2 POTEGA (KWADRAT) TWOJEJ LICZBY TO: " << kwadrat;
+             }
+             break;
